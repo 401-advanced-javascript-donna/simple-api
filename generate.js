4239c5b4faba279cf53579c4166967f1da874cd3
sanvean74 <dlambertpdx@gmail.com>
2019-09-24 14:35:48 -0700

    fixed linting

--- generate.js
+++ generate.js
@@ -2,7 +2,7 @@ module.exports = function() {
   const faker = require('faker');
   const _ = require('lodash');
   return {
-    categories: _.times(100, catId => {
+    categories: _.times(10, catId => {
       return {
         _id: catId, 
         name: faker.random.word(),
@@ -10,7 +10,7 @@ module.exports = function() {
         description: faker.random.words()
       }
     }),
-    products: _.times(100, prodId => {
+    products: _.times(30, prodId => {
       return {
         _id: prodId,
         category: faker.commerce.productMaterial(),
